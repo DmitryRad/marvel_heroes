e784b799ca4bcaf559fb23cda1cbc2031a590b0b
wDmitry <server@internet.ru>
2022-08-03 15:21:41 +0300

    bug fix

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,7 +3,6 @@ import {useHttp} from "../hooks/http.hook";
 const useMarvelService = () => {
     const {loading, request, error, clearError} = useHttp();
 
-
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _apiKey = 'apikey=2af7b51233b93656e631b2e2ae0eaac7\n';
     const _baseOffset = 210;
@@ -13,19 +12,19 @@ const useMarvelService = () => {
         return res.data.results.map(_transformCharacter);
     }
 
-    const getAllComics = async (offset = _baseOffset) => {
-        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
-        return res.data.results.map(_transformComics);
-    }
-
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
+
     const getComic = async (id) => {
-        const res = await request(`${_apiBase}comics?${_apiKey}`);
-        return _transformComics(res.data.results);
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
     }
 
     const _transformCharacter = (char) => {
